84dade04e5b09c7818fa01107468dec2aadd14df
9utae <ddr04099@gmail.com>
2022-02-07 16:19:00 +0900

    make page button

--- src/component/Day.tsx
+++ src/component/Day.tsx
@@ -1,21 +1,64 @@
-import React from 'react'
-import {useParams} from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import {useNavigate, useParams} from "react-router-dom";
 import useFetch from "../hook/useFetch";
+import { IDay } from './DayList';
 import Word, { IWord } from "./Word";
 
 export default function Day() {
     const { day } = useParams<{ day: string }>();
+    const days: IDay[] = useFetch("http://localhost:3002/days")    
     const words: IWord[] = useFetch(`http://localhost:3002/words?day=${day}`);
 
-    return <>
-        <h2>Day {day}</h2>
-        {words.length === 0 && <span>Loading...</span>}
-        <table>
-            <tbody>
-                {words.map(word => (
-                    <Word word={word} key={word.id}/>
-                ))}
-            </tbody>
-        </table>
-    </>
+    const [prevLock, setPrevLock] = useState(false);
+    const [nextLock, setNextLock] = useState(false);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(Number(day) === 1){
+            setPrevLock(true);
+        }
+        else{
+            setPrevLock(false);
+        }
+        if(Number(day) === days.length){
+            setNextLock(true);
+        }
+        else{
+            setNextLock(false);
+        }
+    }, [day, days])
+
+    function prev(){
+        if(!prevLock){
+            navigate(`/day/${Number(day)-1}`);
+        }
+    }
+
+    function next(){
+        if(!nextLock){
+            navigate(`/day/${Number(day)+1}`);
+        }
+    }
+
+    return(
+        <>
+            <div className='word_head'>
+                <button className='page_btn' onClick={prev}
+                style={{ opacity : prevLock ? 0.3 : 1}}> &lt; </button>
+                <h2>Day {day}</h2>
+                <button className='page_btn' onClick={next}
+                style={{ opacity : nextLock ? 0.3 : 1}}> &gt; </button>
+            </div>
+            {words.length === 0 && <span>Loading...</span>}
+            <table>
+                <tbody>
+                    {words.map(word => (
+                        <Word word={word} key={word.id}/>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    )
+    
 }
--- src/component/Word.tsx
+++ src/component/Word.tsx
@@ -60,17 +60,17 @@ export default function Word ({word: w}: IProps) {
 
     return (
         <>
-        <tr className={isDone ? "off" : ""}>
-            <td><input type="checkbox" checked={isDone} onChange={toggleDone}/></td>
-            <td>{word.eng}</td>
-            <td>{isShow && word.kor}</td>
-            <td>
-                <button onClick={toggleShow}>
-                    뜻 {isShow ? "숨기기" : "보기"}
-                </button>
-                <button className="btn_del" onClick={del}>삭제</button>
-            </td>
-        </tr>
+            <tr className={isDone ? "off" : ""}>
+                <td><input type="checkbox" checked={isDone} onChange={toggleDone}/></td>
+                <td>{word.eng}</td>
+                <td>{isShow && word.kor}</td>
+                <td>
+                    <button onClick={toggleShow}>
+                        뜻 {isShow ? "숨기기" : "보기"}
+                    </button>
+                    <button className="btn_del" onClick={del}>삭제</button>
+                </td>
+            </tr>
         </>
     )
 }
