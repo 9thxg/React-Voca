0e33077366f480c608a72fc771dcceb24c520886
9utae <ddr04099@gmail.com>
2022-02-08 23:42:29 +0900

    upgrade delword

--- src/component/DeleteDay.tsx
+++ src/component/DeleteDay.tsx
@@ -1,23 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useFetch from "../hook/useFetch";
 import {useNavigate} from "react-router-dom";
 import { IDay } from "./DayList";
+import { IWord } from "./Word";
 
 export default function DeleteDay() {
     const days: IDay[] = useFetch("http://localhost:3002/days");
+    const words: IWord[] = useFetch(`http://localhost:3002/words?day=${days.length}`);
+    
+    console.log(words);
 
     const navigate = useNavigate();
 
+    const DelWord = (word: number) => {
+        return new Promise((resolve) => {
+            fetch(`http://localhost:3002/words/${word}`, {
+                method : 'DELETE',
+            }).then((res) => {
+                resolve(res);
+            })
+        })
+    }
+
     function DelDay() {
         if(window.confirm("정말 삭제하시겠습니까?(내용도 함께 사라집니다.)")){
-            fetch(`http://localhost:3002/days/${days.length}`, {
-                method : 'DELETE',
-            }).then(res => {
-                if(res.ok){
-                    alert("삭제되었습니다 !");
-                    navigate(`/`);
-                }
-            })
+            if(words.length === 0){
+                fetch(`http://localhost:3002/days/${days.length}`, {
+                    method : 'DELETE',
+                }).then(res => {
+                    if(res.ok){
+                        alert("삭제되었습니다 !");
+                        navigate(`/`);
+                    }
+                })
+            }
+            else{
+                // words.map(word => (
+                //     fetch(`http://localhost:3002/words/${word.id}`, {
+                //         method : 'DELETE',
+                //     })
+                // ))
+                words.forEach(async (word) => await DelWord(word.id));
+
+                fetch(`http://localhost:3002/days/${days.length}`, {
+                    method : 'DELETE',
+                }).then(res => {
+                    if(res.ok){
+                        alert("삭제되었습니다 !");
+                        navigate(`/`);
+                    }
+                })
+            }
         }
     }
 
