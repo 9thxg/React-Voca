6935c54a7f0348f26f0aa7324975f1230584bc9f
9utae <ddr04099@gmail.com>
2022-02-09 21:08:32 +0900

    complete DelDay

--- src/component/DeleteDay.tsx
+++ src/component/DeleteDay.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import useFetch from "../hook/useFetch";
 import {useNavigate} from "react-router-dom";
 import { IDay } from "./DayList";
@@ -7,49 +7,72 @@ import { IWord } from "./Word";
 export default function DeleteDay() {
     const days: IDay[] = useFetch("http://localhost:3002/days");
     const words: IWord[] = useFetch(`http://localhost:3002/words?day=${days.length}`);
-    
-    console.log(words);
 
     const navigate = useNavigate();
 
-    const DelWord = (word: number) => {
-        return new Promise((resolve) => {
-            fetch(`http://localhost:3002/words/${word}`, {
+    const DelWord = () => {
+        const wordId = words.map(word => word.id);
+
+        //console.log(wordId.toString());
+
+        return (
+            fetch(`http://localhost:3002/words/${wordId.toString()}`, {
                 method : 'DELETE',
-            }).then((res) => {
-                resolve(res);
             })
+        )
+        
+        // return new Promise((resolve) => {
+        //     fetch(`http://localhost:3002/words/${wordId}`, {
+        //         method : 'DELETE',
+        //     }).then((res) => {
+        //         resolve(res);
+        //     })
+        // })
+    }
+
+    const DelDay = (day: number) => {
+        fetch(`http://localhost:3002/days/${day}`, {
+            method : 'DELETE',
+        }).then(res => {
+            if(res.ok){
+                alert("삭제되었습니다 !");
+                navigate(`/`);
+            }
         })
     }
 
-    function DelDay() {
+    function Delete() {
         if(window.confirm("정말 삭제하시겠습니까?(내용도 함께 사라집니다.)")){
             if(words.length === 0){
-                fetch(`http://localhost:3002/days/${days.length}`, {
-                    method : 'DELETE',
-                }).then(res => {
-                    if(res.ok){
-                        alert("삭제되었습니다 !");
-                        navigate(`/`);
-                    }
-                })
+                DelDay(days.length);
             }
             else{
+                DelWord()
+                .then(res => {
+                    if(res.ok){
+                        DelDay(days.length);
+                    }
+                })
                 // words.map(word => (
                 //     fetch(`http://localhost:3002/words/${word.id}`, {
                 //         method : 'DELETE',
                 //     })
                 // ))
-                words.forEach(async (word) => await DelWord(word.id));
+                // words.forEach(async (word) => await DelWord(word.id));
 
-                fetch(`http://localhost:3002/days/${days.length}`, {
-                    method : 'DELETE',
-                }).then(res => {
-                    if(res.ok){
-                        alert("삭제되었습니다 !");
-                        navigate(`/`);
-                    }
-                })
+                // Promise.all([words.map(async (word) => await DelWord(word.id))])
+                // .then((res) => {
+                //     console.log(res);
+                // })
+
+                // fetch(`http://localhost:3002/days/${days.length}`, {
+                //     method : 'DELETE',
+                // }).then(res => {
+                //     if(res.ok){
+                //         alert("삭제되었습니다 !");
+                //         navigate(`/`);
+                //     }
+                // })
             }
         }
     }
@@ -57,7 +80,7 @@ export default function DeleteDay() {
     return (
         <div>
             <h2>현재일수 : {days.length}일</h2>
-            <button className='btn_del' onClick={DelDay}>Day 삭제</button>
+            <button className='btn_del' onClick={Delete}>Day 삭제</button>
         </div>
     )
 };
